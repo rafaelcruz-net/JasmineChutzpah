5bf026d4b1f180e8e9e4286be8cf1b1955a8bf01
Rafael Cruz <rcruz@mundipagg.com>
2016-07-18 19:57:33 -0300

    Initial Commit

---
+++ Chutzpah/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chutzpah.MvcApplication" Language="C#" %>
---
+++ Chutzpah/Scripts/calculadora.js
@@ -0,0 +1,21 @@
+﻿var Calculadora  = function () {
+
+    this.Soma = function (x, y) {
+        return x + y;
+    };
+
+    this.Subtrair = function (x, y) {
+        return x = y;
+    };
+
+    this.Multiplicar = function (x, y) {
+        return x * y;
+    };
+
+    this.Dividir = function (x, y) {
+        if (y === 0)
+            throw "Denominador deve ser maior que zero";
+
+        return x / y;
+    };
+};
